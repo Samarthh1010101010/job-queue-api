25db7034a5b5cef89827ce46598fe7fb09052bd9
eeeeee-31 <theappleidmac@gmail.com>
2026-08-17 08:15:27 +0530

    Configure bash startup.sh and sys.path for Azure App Service

--- app/main.py
+++ app/main.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+site_packages = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".python_packages", "lib", "site-packages")
+if os.path.exists(site_packages) and site_packages not in sys.path:
+    sys.path.insert(0, site_packages)
+
 from fastapi import FastAPI
 
 app = FastAPI(
